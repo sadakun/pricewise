0195210937c4a4ec6ec8fe0773f4912505b63d94
sadakun <sm123kuncoro@gmail.com>
2024-08-16 17:35:41 +0700

    declare logic for server action, and set up for scrape and save it to db

---
+++ lib/actions/index.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { scrapeAmazonProduct } from "../scraper";
+
+export async function scrapeAndStoreProduct(productUrl: string) {
+  // check if no product url then exit
+  if (!productUrl) return;
+
+  try {
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+  } catch (error: any) {
+    throw new Error("Failed to create/update product: ${error.message}");
+  }
+}
---
+++ lib/scraper/index.ts
@@ -0,0 +1,5 @@
+export async function scrapeAmazonProduct(url: string) {
+  if (!url) return;
+
+  // BrightData proxy configuration
+}
