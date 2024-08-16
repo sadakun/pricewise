4e92bfb172ad8c00b1fbf57f67be28744e98a94e
sadakun <sm123kuncoro@gmail.com>
2024-08-16 16:12:53 +0700

    update hero carousel

--- components/HeroCarousel.tsx
+++ components/HeroCarousel.tsx
@@ -14,7 +14,7 @@ const heroImages = [
 
 const HeroCarousel = () => {
   return (
-    <div>
+    <div className="hero-carousel">
       <Carousel
         showThumbs={false}
         autoPlay
@@ -34,6 +34,14 @@ const HeroCarousel = () => {
           />
         ))}
       </Carousel>
+
+      <Image
+        src="assets/icons/hand-drawn-arrow.svg"
+        alt="arrow"
+        width={175}
+        height={175}
+        className="max-xl:hidden absolute -left-[15%] bottom-0 z-0"
+      />
     </div>
   );
 };
