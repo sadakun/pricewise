f61f7b7d732c5cfbbdd93605f73741dff35c897f
sadakun <sm123kuncoro@gmail.com>
2024-08-17 17:59:28 +0700

    implement cheerio to understand the data we scraping

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
+import { extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -25,8 +26,30 @@ export async function scrapeAmazonProduct(url: string) {
   try {
     // Fetch the procut page
     const response = await axios.get(url, options);
+    const $ = cheerio.load(response.data);
 
-    console.log(response.data);
+    // Extract the product title
+    const title = $("#productTitle").text().trim();
+    const currentPrice = extractPrice(
+      $(".priceToPay span.a-price-whole"),
+      $(".a.size.base.a-color-price"),
+      $(".a-button-selected .a-color-base"),
+      // $(".a-price.a-text-price"),
+      $(".a-price.a-text-price.a-size-medium.apexPriceToPay")
+    );
+
+    const originalPrice = extractPrice(
+      // $("#priceblock_ourprice"),
+      // $(".a-price.a-text-price span.a-offscreen"),
+      // $("#priceblock_dealprice"),
+      $(".a-price.a-text-price.a-size-base")
+    );
+
+    const outOfStock =
+      $("#availability span").text().trim().toLowerCase() ===
+      "currently unavailable";
+
+    console.log({ title, currentPrice, originalPrice });
   } catch (error: any) {
     throw new Error("Failed to scrape product: ${error.message}");
   }
