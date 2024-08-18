a20e16b92a0952d5dea512b8d8f2510a36e89bab
sadakun <sm123kuncoro@gmail.com>
2024-08-18 18:40:49 +0700

    display product on home

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import HeroCarousel from "@/components/HeroCarousel";
+import ProductCard from "@/components/ProductCard";
 import Searchbar from "@/components/Searchbar";
+import { getAllProduct } from "@/lib/actions";
 import Image from "next/image";
 
-const Home = () => {
+const Home = async () => {
+  const allProducts = await getAllProduct();
+
   return (
     <>
       <section className="px-6 md:px-20 py-24 border-2 border-red-500">
@@ -35,8 +39,8 @@ const Home = () => {
         <h2 className="section-text">Trending</h2>
 
         <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {["Apple Iphone 15", "Book", "Sneakers"].map((product) => (
-            <div>{product}</div>
+          {allProducts?.map((product) => (
+            <ProductCard key={product._id} product={product} />
           ))}
         </div>
       </section>
