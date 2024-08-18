426013be2be8eed6716efaf2a7eb7fe567277f8a
sadakun <sm123kuncoro@gmail.com>
2024-08-18 14:17:37 +0700

    Construct data object with scraped information

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractCurrency, extractPrice } from "../utils";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
 
 export async function scrapeAmazonProduct(url: string) {
   if (!url) return;
@@ -60,6 +60,8 @@ export async function scrapeAmazonProduct(url: string) {
 
     const discountRate = $(".savingsPercentage").text().replace(/[-%]/g, "");
 
+    const description = extractDescription($);
+
     // console.log({
     //   title,
     //   currentPrice,
@@ -77,17 +79,22 @@ export async function scrapeAmazonProduct(url: string) {
       currency: currency || "$",
       image: imageUrls[0],
       title,
-      currentPrice: Number(currentPrice),
-      originalPrice: Number(originalPrice),
+      currentPrice: Number(currentPrice) || Number(originalPrice),
+      originalPrice: Number(originalPrice) || Number(currentPrice),
       priceHistory: [],
       discountRate: Number(discountRate),
       category: "category",
       reviewsCount: 100,
       stars: 4.5,
       isOutOfStock: outOfStock,
+      description,
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      average: Number(currentPrice) || Number(originalPrice),
     };
 
-    console.log(data);
+    // console.log(data);
+    return data;
   } catch (error: any) {
     throw new Error("Failed to scrape product: ${error.message}");
   }
