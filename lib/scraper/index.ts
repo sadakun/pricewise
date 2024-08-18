fc2b2eab66a9f2c0ae811aed2ee37f26776f36d8
sadakun <sm123kuncoro@gmail.com>
2024-08-18 17:15:02 +0700

    create a schema or model to recognize by mongodb, and store product in mongodb

--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -90,7 +90,7 @@ export async function scrapeAmazonProduct(url: string) {
       description,
       lowestPrice: Number(currentPrice) || Number(originalPrice),
       highestPrice: Number(originalPrice) || Number(currentPrice),
-      average: Number(currentPrice) || Number(originalPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
     };
 
     // console.log(data);
